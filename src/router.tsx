cd18812572ba9450596652924edd4940c6a0c4ca
Uttam <shettyuttam010@gmail.com>
2026-09-07 13:39:48 +0530

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import { routeTree } from "./routeTree.gen";
 
 export const router = createRouter({
   routeTree,
+  basepath: "/UTTAM-s-PORTFOLIO",
 });
 
 declare module "@tanstack/react-router" {
